7876c588c6a3babe7485356e687c183ab2b97ad2
Lcj Bow <160986475+PFStu@users.noreply.github.com>
2024-10-07 12:46:30 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,31 @@
+
+window.onload = function(){
+    document.getElementById("board").style.display = "none";
+    document.getElementsByClassName("filter")[0].style.display = "none";
+    showBoard();
+}
+
+function showBoard() {
+    const lastShown = localStorage.getItem('boardLastShown');
+    const currentTime = new Date().getTime();
+
+    if (!lastShown || (currentTime - parseInt(lastShown)) > 30 * 24 * 60 * 60 * 1000) {
+        document.getElementById("board").style.display = "block";
+        document.getElementsByClassName("filter")[0].style.display = "block";
+        localStorage.setItem('boardLastShown', currentTime);
+    }
+}
+
+function closeboard() {
+    document.getElementById("board").style.display = "none";
+    document.getElementsByClassName("filter")[0].style.display = "none";
+}
+
 const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.querySelector('.nav-links');
-const menuItems = navLinks.querySelectorAll('.nav-links li');
 
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-});
-// 原始中文和英文文本
-const originalTexts = [
-    '主页',
-    '关于',
-    '项目',
-    '工具',
-    '资源',
-    '软件'
-];
-
-const englishTexts = [
-    'Home',
-    'About',
-    'Proj',
-    'Tools',
-    'Res',
-    'Soft'
-];
-
-menuItems.forEach((item, index) => {
-    const link = item.querySelector('a');
-    const originalText = originalTexts[index];
-    const englishText = englishTexts[index];
-
-    // 设置悬浮时的效果
-    item.addEventListener('mouseenter', () => {
-        link.classList.add('hidden');
-        setTimeout(() => {
-            link.textContent = englishText;
-            link.classList.remove('hidden');
-        }, 300); // 等待过渡完成后再切换文本
-    });
-
-    // 鼠标移开时变回中文
-    item.addEventListener('mouseleave', () => {
-        link.classList.add('hidden');
-        setTimeout(() => {
-            link.textContent = originalText;
-            link.classList.remove('hidden');
-        }, 300); // 等待过渡完成后再切换文本
-    });
 });
 let theme = 'light';
 document.getElementById('themeToggle').addEventListener('click', () => {
@@ -57,15 +37,8 @@ document.getElementById('themeToggle').addEventListener('click', () => {
         let theme = 'light';
     }
 });
+function copyqq(){
+    navigator.clipboard.writeText("929578771");
+    alert("QQ号已复制到粘贴板");
+}
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // 防止表单提交
-    
-    const message = encodeURIComponent(document.getElementById('message').value);
-    const email = 'mc_bow@outlook.com';
-    const subject = '留言内容';
-    const body = `留言内容：${message}`;
-    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
-    
-    window.location.href = mailtoLink; // 跳转到邮箱
-});
