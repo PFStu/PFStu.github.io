fa4b674f43c9dc7abb17692a31c5b009d8940e51
ILoveScratch <yaowendao2023@qq.com>
2024-12-10 22:58:03 +0800

    添加暗色模式

--- js/scripts.js
+++ js/scripts.js
@@ -155,4 +155,28 @@ document.querySelector('.music-next-btn').addEventListener('click', () => {
 });
 
 
+
+const themeToggleButton = document.getElementById('themeToggle');
+const body = document.body;
+
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.toggle('dark-theme', currentTheme === 'dark');
+}
+
+
+themeToggleButton.addEventListener('click', () => {
+
+    body.classList.toggle('light-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+
+
 loadTrack();
