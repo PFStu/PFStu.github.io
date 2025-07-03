519bd5fe1335715ea776177f1998e956316ff938
PFStu <ch_bow@126.com>
2025-07-03 20:25:40 +0800

    BYD Netlify

--- app/page.tsx
+++ app/page.tsx
@@ -21,16 +21,4 @@ const Page = () => {
   );
 };
 
-const RedstoneLamp = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-yellow-300 mb-4 font-minecraft">红石灯</h1>
-        <p className="text-gray-300">这是红石电路的最后一步</p>
-      </div>
-    </div>
-  );
-};
-
 export default Page;
-export { RedstoneLamp };
